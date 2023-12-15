9fd31ab9fdc2cd020a843279dc16701fbdc22585
poka <khs0826@dsm.hs.kr>
2023-12-15 18:17:06 +0900

    add command

--- pkg/message.go
+++ pkg/message.go
@@ -1,7 +1,10 @@
 package pkg
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"image/color"
+	"image/png"
 	"log"
 	"os"
 	"regexp"
@@ -32,7 +35,75 @@ func MessageCreate(s *discordgo.Session, msg *discordgo.MessageCreate) {
 	r2, _ := regexp.Compile("(?i)(n[ei]+g+[ea]*r*|n word)")
 	if r1.MatchString(msg.Content) || r2.MatchString(msg.Content) {
 		log.Printf("%s이 %s라고 말함", msg.Author.Username, msg.Content)
-		s.ChannelMessageSend(msg.ChannelID, "목화재배!")
-		s.ChannelMessageSend(msg.ChannelID, gif)
+		f, _ := os.Open("./image/dallas.gif")
+		s.ChannelMessageSendComplex(msg.ChannelID, &discordgo.MessageSend{
+			Content: "목화재배!",
+			File: &discordgo.File{
+				Name:        "cotton.gif",
+				ContentType: "image/gif",
+				Reader:      f,
+			},
+		})
+	}
+
+	if words[0] == "!지지관계" {
+		nameRegxep, _ := regexp.Compile("^김현석$")
+		var username string
+		if nameRegxep.MatchString(words[1]) {
+			username = "조승우"
+		} else {
+			username = words[0]
+		}
+
+		var builder strings.Builder
+		builder.WriteString(fmt.Sprintf("오늘부터 지지관계에서 벗어나\n쓱규와(과) %s는 한몸으로 일체가 된다\n쓱규에 대한 공격은 %s에 대한 공격으로 간주한다\n\n", username, username))
+
+		for _, fans := range []string{"70억", "1억", "천만", "백", "한"} {
+			builder.WriteString(fmt.Sprintf("세상에 %s 명의 쓱규 팬이 있다면, %s는 그들 중 한 명일 것이다.\n", fans, username))
+		}
+		builder.WriteString(fmt.Sprintf("세상에 단 한 명의 쓱규 팬도 없다면, %s는 그제서야 이 세상에 없는 것이다.\n\n", username))
+
+		for _, attribute := range []string{"사랑", "빛", "어둠", "삶", "기쁨", "슬픔", "안식", "영혼"} {
+			builder.WriteString(fmt.Sprintf("쓱규, %s의 %s.\n", username, attribute))
+		}
+		builder.WriteString(fmt.Sprintf("쓱규, %s.\n%s.", username, username))
+
+		embed := &discordgo.MessageEmbed{
+			Title:       "# 지지관계",
+			Description: builder.String(),
+			Color:       0x00ff00,
+		}
+		s.ChannelMessageSendEmbed(msg.ChannelID, embed)
+	}
+}
+
+func test() {
+
+	catFile, err := os.Open("/home/arkaprabham/Documents/Journal_Dev/Golang/github.com/image-op/cat.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer catFile.Close()
+
+	// Consider using the general image.Decode as it can sniff and decode any registered image format.
+	img, err := png.Decode(catFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(img)
+
+	levels := []string{" ", "░", "▒", "▓", "█"}
+
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+			level := c.Y / 51 // 51 * 5 = 255
+			if level == 5 {
+				level--
+			}
+			fmt.Print(levels[level])
+		}
+		fmt.Print("\n")
 	}
 }
